f9592b214a0f8ca03e079b03f09ed15744a3926e
cherryvue <yetiasg@gmail.com>
2021-12-17 12:00:27 +0100

    Connect mongodb module, Connect to mongodb database, Attach survey schema to db module

--- team-fingerprints-api/src/app.module.ts
+++ team-fingerprints-api/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SurveyModule } from './survey/survey.module';
 
 @Module({
-  imports: [SurveyModule],
+  imports: [
+    SurveyModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/surveys'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- team-fingerprints-api/src/survey/survey.module.ts
+++ team-fingerprints-api/src/survey/survey.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Survey, SurveySchema } from 'src/entities/survey.entity';
 import { SurveyController } from './survey.controller';
 import { SurveyService } from './survey.service';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Survey.name,
+        schema: SurveySchema,
+      },
+    ]),
+  ],
   controllers: [SurveyController],
   providers: [SurveyService],
 })

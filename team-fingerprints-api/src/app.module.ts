9bed130b948f3802d99f70904e713f98c4545ba7
cherryvue <yetiasg@gmail.com>
2021-12-17 16:11:40 +0100

    Divide survey.module into smaller and nested modules, Apply routing in app.module

--- team-fingerprints-api/src/app.module.ts
+++ team-fingerprints-api/src/app.module.ts
@@ -1,13 +1,46 @@
 import { Module } from '@nestjs/common';
+import { RouterModule, Routes } from '@nestjs/core';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CategoryModule } from './survey/category/category.module';
+import { QuestionModule } from './survey/category/trend/question/question.module';
+import { TrendModule } from './survey/category/trend/trend.module';
 import { SurveyModule } from './survey/survey.module';
+
+const routes: Routes = [
+  {
+    path: 'survey',
+    module: SurveyModule,
+    children: [
+      {
+        path: '/:surveyId/category',
+        module: CategoryModule,
+        children: [
+          {
+            path: '/:categoryId/trend',
+            module: TrendModule,
+            children: [
+              {
+                path: '/:trendId/question/',
+                module: QuestionModule,
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+];
 
 @Module({
   imports: [
-    SurveyModule,
     MongooseModule.forRoot('mongodb://localhost:27017/surveys'),
+    SurveyModule,
+    CategoryModule,
+    TrendModule,
+    QuestionModule,
+    RouterModule.register(routes),
   ],
   controllers: [AppController],
   providers: [AppService],

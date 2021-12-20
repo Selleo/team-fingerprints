21e61618840d295038bee5b0a5d65e1c7db28b2d
Wojciech Bożek <bozek.wojciech@gmail.com>
2021-12-20 16:02:03 +0100

    Add for for creating surveys

--- team-fingerprints-web/src/index.tsx
+++ team-fingerprints-web/src/index.tsx
@@ -4,6 +4,10 @@ import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import App from "./App";
 
+import axios from "axios";
+
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
--- team-fingerprints-web/src/routes/Surveys/Surveys.tsx
+++ team-fingerprints-web/src/routes/Surveys/Surveys.tsx
@@ -1,17 +1,20 @@
-import { Button, Skeleton, Table } from "@mantine/core";
-import React from "react";
+import { Button, Modal, Skeleton, Table } from "@mantine/core";
+import React, { useState } from "react";
 import { useQuery } from "react-query";
 import times from "lodash/times";
 import isArray from "lodash/isArray";
+import isEmpty from "lodash/isEmpty";
 
 import { useStyles } from "./styles";
 import SurveyItem from "../../components/_Survey/SurveyItem";
+import CreateSurveyForm from "../../components/CreateSurveyForm";
 
 const Surveys = () => {
   const { classes } = useStyles();
+  const [createModalVisible, setCreateModalVisible] = useState(false);
 
-  const { isLoading, error, data } = useQuery("surveys", () =>
-    fetch(`${process.env.REACT_APP_API_URL}/surveys`).then((res) => res.json())
+  const { isLoading, error, data } = useQuery("surveysAll", () =>
+    fetch(`${process.env.REACT_APP_API_URL}/survey`).then((res) => res.json())
   );
   if (isLoading)
     return (
@@ -23,26 +26,43 @@ const Surveys = () => {
     );
   if (error) return <div>'An error has occurred: ' + console.error;</div>;
 
-  console.log(data);
-
   return (
     <>
       <div className={classes.header}>
         <h1 className={classes.headerTitle}>Surveys</h1>
-        <Button className={classes.addButton}>Add new survey</Button>
+        <Button
+          onClick={() => setCreateModalVisible(true)}
+          className={classes.addButton}
+        >
+          Add new survey
+        </Button>
       </div>
       <Table>
         <thead>
           <tr>
-            <th>Title</th>
+            <th>title</th>
             <th>is public</th>
-            <th>Actions</th>
+            <th>actions</th>
           </tr>
         </thead>
-        <tbody>
-          {isArray(data) && data?.map((item) => <SurveyItem item={item} />)}
-        </tbody>
+        {isArray(data) &&
+          (isEmpty(data) ? (
+            <span className={classes.emptyCopy}>No surveys yet</span>
+          ) : (
+            <tbody>
+              {data?.map((item) => (
+                <SurveyItem item={item} />
+              ))}
+            </tbody>
+          ))}
       </Table>
+      <Modal
+        opened={createModalVisible}
+        onClose={() => setCreateModalVisible(false)}
+        title="Create Survey"
+      >
+        <CreateSurveyForm onClose={() => setCreateModalVisible(false)} />
+      </Modal>
     </>
   );
 };

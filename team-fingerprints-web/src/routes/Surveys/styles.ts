69f7aafa0c3a76089a14d98fd0d8ae094a320a35
Wojciech Bożek <bozek.wojciech@gmail.com>
2021-12-20 14:38:20 +0100

    Add list of surveys

---
+++ team-fingerprints-web/src/routes/Surveys/styles.ts
@@ -0,0 +1,14 @@
+import { createStyles } from "@mantine/core";
+
+export const useStyles = createStyles((theme) => ({
+  addButton: {
+    flex: 0,
+    margin: "20px",
+  },
+  header: {
+    display: "flex",
+  },
+  headerTitle: {
+    flex: 1,
+  },
+}));

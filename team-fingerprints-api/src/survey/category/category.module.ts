e78568bc2a74017929be57a30a73e6b65d4fe336
cherryvue <yetiasg@gmail.com>
2021-12-20 11:12:40 +0100

    Import mongodb module to question, trend and category modules

--- team-fingerprints-api/src/survey/category/category.module.ts
+++ team-fingerprints-api/src/survey/category/category.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Survey, SurveySchema } from 'src/entities/survey.entity';
 import { CategoryController } from './category.controller';
 import { CategoryService } from './category.service';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Survey.name,
+        schema: SurveySchema,
+      },
+    ]),
+  ],
   controllers: [CategoryController],
   providers: [CategoryService],
 })
--- team-fingerprints-api/src/survey/category/trend/trend.module.ts
+++ team-fingerprints-api/src/survey/category/trend/trend.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TrendController } from './trend.controller';
 import { TrendService } from './trend.service';
-import { QuestionModule } from './question/question.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Survey, SurveySchema } from 'src/entities/survey.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Survey.name,
+        schema: SurveySchema,
+      },
+    ]),
+  ],
   controllers: [TrendController],
   providers: [TrendService],
-  imports: [QuestionModule],
 })
 export class TrendModule {}

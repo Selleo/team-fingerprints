e5ee5fe8304b78446dfa6fce6c65a514db2d7435
cherryvue <yetiasg@gmail.com>
2021-12-23 13:13:54 +0100

    Create user module, create schema entities and connect to database, connect module to root module

---
+++ team-fingerprints-api/src/users/entities/user.entity.ts
@@ -0,0 +1,39 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import { UserRole } from '../Roles/UserRoles.enum';
+import {
+  UserSurveyResponseSchema,
+  UserSurveyResponse,
+} from './userSurveyResponse.entity';
+
+@Schema({ autoIndex: true, timestamps: true })
+export class User extends Document {
+  @Prop({ required: true })
+  firstName: string;
+
+  @Prop({ required: true })
+  lastName: string;
+
+  @Prop({ required: true })
+  email: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop({ default: UserRole.USER, required: true })
+  role: UserRole;
+
+  @Prop({ default: '' })
+  team?: string;
+
+  @Prop({ default: '' })
+  passwordResetToken?: string;
+
+  @Prop({ default: '' })
+  url?: string;
+
+  @Prop({ type: [UserSurveyResponseSchema], default: [], required: true })
+  surveysResponses: UserSurveyResponse[];
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
---
+++ team-fingerprints-api/src/users/entities/userSurveyResponse.entity.ts
@@ -0,0 +1,16 @@
+import { Schema, SchemaFactory, Prop } from '@nestjs/mongoose';
+import {
+  QuestionResponseSchema,
+  QuestionResponse,
+} from './questionResponse.entity';
+
+@Schema({ autoIndex: true })
+export class UserSurveyResponse {
+  @Prop({ required: true })
+  surveyId: string;
+
+  @Prop({ type: [QuestionResponseSchema], default: [], required: true })
+  response: QuestionResponse[];
+}
+export const UserSurveyResponseSchema =
+  SchemaFactory.createForClass(UserSurveyResponse);

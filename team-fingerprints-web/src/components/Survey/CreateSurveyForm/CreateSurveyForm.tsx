a29e9d6406cca58f5f23c898d167477e23170514
Wojciech Bożek <bozek.wojciech@gmail.com>
2021-12-23 11:39:10 +0100

    Fix api calls

--- team-fingerprints-web/src/components/Survey/CreateSurveyForm/CreateSurveyForm.tsx
+++ team-fingerprints-web/src/components/Survey/CreateSurveyForm/CreateSurveyForm.tsx
@@ -4,13 +4,14 @@ import { useMutation } from "react-query";
 import { useStyles } from "./styles";
 import axios from "axios";
 import { queryClient } from "../../../App";
+import { Survey } from "../../../types/models";
 
 const CreateSurveyForm = ({ onClose }: { onClose: () => void }) => {
   const { classes } = useStyles();
 
   const mutation = useMutation(
-    (newSurvey) => {
-      return axios.post("/survey", newSurvey).then(onClose);
+    (newSurvey: Partial<Survey>) => {
+      return axios.post<Partial<Survey>>("/survey", newSurvey).then(onClose);
     },
     {
       onSuccess: () => {
@@ -19,13 +20,9 @@ const CreateSurveyForm = ({ onClose }: { onClose: () => void }) => {
     }
   );
 
-  const onSubmit = (data: any) => {
-    mutation.mutate(data);
-  };
-
-  const { handleSubmit, handleChange } = useFormik({
-    initialValues: { data: { title: "" } },
-    onSubmit,
+  const { handleSubmit, handleChange } = useFormik<Partial<Survey>>({
+    initialValues: { title: "" },
+    onSubmit: (val) => mutation.mutate(val),
   });
 
   return (
@@ -34,7 +31,7 @@ const CreateSurveyForm = ({ onClose }: { onClose: () => void }) => {
         required
         label="Survey title"
         placeholder="Survey name"
-        onChange={handleChange("data.title")}
+        onChange={handleChange("title")}
       />
 
       <Button className={classes.submitButton} type="submit">

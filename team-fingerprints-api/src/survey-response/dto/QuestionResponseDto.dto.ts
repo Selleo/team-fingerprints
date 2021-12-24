0e86e20952288576b18b87e4a86e9d8e6f2e3707
cherryvue <yetiasg@gmail.com>
2021-12-24 11:50:49 +0100

    Separate userSurveyResponse routes to other module

---
+++ team-fingerprints-api/src/survey-response/dto/QuestionResponseDto.dto.ts
@@ -0,0 +1,12 @@
+import { IsDefined, IsMongoId, IsNumber, IsString } from 'class-validator';
+
+export class QuestionResponseDto {
+  @IsString()
+  @IsMongoId()
+  @IsDefined()
+  readonly questionId: string;
+
+  @IsNumber()
+  @IsDefined()
+  readonly value: string;
+}

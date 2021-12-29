943cff9438d682ea51e098af8447d613ebe5d953
cherryvue <yetiasg@gmail.com>
2021-12-29 12:23:00 +0100

    Apply config based on env files

--- team-fingerprints-api/src/main.ts
+++ team-fingerprints-api/src/main.ts
@@ -1,4 +1,5 @@
 import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
@@ -24,6 +25,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  const port = app.get(ConfigService).get('PORT');
+  await app.listen(port);
 }
 bootstrap();

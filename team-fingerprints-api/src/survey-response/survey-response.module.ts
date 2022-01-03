030f37bb9ee3775de829a725b5d9ac2713db9173
cherryvue <yetiasg@gmail.com>
2022-01-03 09:42:08 +0100

    WIP: Work on counting points module

--- team-fingerprints-api/src/survey-response/survey-response.module.ts
+++ team-fingerprints-api/src/survey-response/survey-response.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { SurveySummarizeModule } from 'src/survey-summarize/survey-summarize.module';
 import { User, UserSchema } from 'src/users/entities/user.entity';
 import { SurveyResponseController } from './survey-response.controller';
 import { SurveyResponseService } from './survey-response.service';
 
 @Module({
   imports: [
+    SurveySummarizeModule,
     MongooseModule.forFeature([
       {
         name: User.name,

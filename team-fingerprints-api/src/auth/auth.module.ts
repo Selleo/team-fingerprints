fd6e9795dbcd32cfa4f275392157d0149abfaf85
cherryvue <yetiasg@gmail.com>
2022-01-19 16:08:48 +0100

    WIP: Create auth module with /profile endpoint

--- team-fingerprints-api/src/auth/auth.module.ts
+++ team-fingerprints-api/src/auth/auth.module.ts
@@ -2,13 +2,18 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
 import { PassportModule } from '@nestjs/passport';
-import { JwtAuthGuard } from 'src/common/guards/JwtAuthGuard.guard';
+import { JwtAuthGuard } from 'src/auth/guards/JwtAuthGuard.guard';
 import { JwtStrategy } from './jwt.strategy';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  imports: [PassportModule.register({ defaultStrategy: 'jwt' }), ConfigModule],
+  imports: [
+    ConfigModule,
+    UsersModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+  ],
   providers: [
     {
       provide: APP_GUARD,

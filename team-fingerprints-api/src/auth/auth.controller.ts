e6c58ec7e1c0dadf3d0599a9203797aa75f22433
cherryvue <yetiasg@gmail.com>
2022-01-19 16:08:48 +0100

    WIP: Create auth module with /profile endpoint

--- team-fingerprints-api/src/auth/auth.controller.ts
+++ team-fingerprints-api/src/auth/auth.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
-
+import { Controller, Get } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { AuthService } from './auth.service';
+@ApiTags('auth')
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Get()
+  async getProfile(userId: string) {
+    return await this.authService.getProfile(userId);
+  }
+}

07d9a4a5fe5ade44a0c3a8c649bc49303016f22f
cherryvue <yetiasg@gmail.com>
2022-01-19 16:08:48 +0100

    WIP: Create auth module with /profile endpoint

--- team-fingerprints-api/src/auth/auth.service.ts
+++ team-fingerprints-api/src/auth/auth.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  async getProfile(userId: string) {
+    return userId;
+  }
+}

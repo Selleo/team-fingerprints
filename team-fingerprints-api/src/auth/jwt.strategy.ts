7c1a9849d1945ae9a0b08c7f09e11f804e337e99
cherryvue <yetiasg@gmail.com>
2022-01-19 16:13:40 +0100

    Add ability to create user if not exists, Create decorator which fetch userId form requestt, Modify user entity

--- team-fingerprints-api/src/auth/jwt.strategy.ts
+++ team-fingerprints-api/src/auth/jwt.strategy.ts
@@ -1,14 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { passportJwtSecret } from 'jwks-rsa';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import * as dotenv from 'dotenv';
+import { UsersService } from 'src/users/users.service';
+import { ConfigService } from '@nestjs/config';
+import * as request from 'request';
 
 dotenv.config();
 
+interface authPayload {
+  iss: string;
+  sub: string;
+  aud: [string, string];
+  iat: number;
+  exp: number;
+  azp: string;
+  scope: string;
+}
+
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(
+    private readonly userService: UsersService,
+    private readonly configService: ConfigService,
+  ) {
     super({
       secretOrKeyProvider: passportJwtSecret({
         cache: true,
@@ -24,7 +40,41 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  validate(payload: unknown): unknown {
-    return payload;
+  async validate(payload: authPayload) {
+    const options = (id: string) => ({
+      method: 'GET',
+      url: `https://dev-llkte41m.us.auth0.com/api/v2/users/${id}`,
+      headers: {
+        authorization: `Bearer ${this.configService.get<string>(
+          'AUTH0_MANAGEMENT_API_TOKEN',
+        )}`,
+      },
+    });
+
+    const { sub } = payload;
+
+    const user = await this.userService.getUserByAuthId(sub);
+    if (user) return user;
+    else if (!user) {
+      let newUser = undefined;
+      request(options(sub), async (err, res, body) => {
+        if (err) return new BadRequestException();
+        const {
+          email,
+          family_name: firstName,
+          given_name: lastName,
+          picture,
+          pictureUrl,
+        } = JSON.parse(body);
+        newUser = await this.userService.createUser({
+          authId: sub,
+          email,
+          pictureUrl: pictureUrl || picture || undefined,
+          firstName,
+          lastName,
+        });
+      });
+      return await newUser.save();
+    } else return new BadRequestException('sth went wrong');
   }
 }

791451789aa89c0de86d99a1a0ef30fcac3750cb
cherryvue <yetiasg@gmail.com>
2022-01-21 12:53:58 +0100

    Update entities

--- team-fingerprints-api/src/users/entities/userSurveyAnswer.entity.ts
+++ team-fingerprints-api/src/users/entities/userSurveyAnswer.entity.ts
@@ -1,4 +1,5 @@
 import { Schema, SchemaFactory, Prop } from '@nestjs/mongoose';
+import { SurveyCompleteStatus } from 'src/survey-answer/survey-answer.type';
 import { QuestionAnswerSchema, QuestionAnswer } from './questionAnswer.entity';
 
 @Schema({ _id: false })
@@ -6,8 +7,14 @@ export class UserSurveyAnswer {
   @Prop({ required: true })
   surveyId: string;
 
+  @Prop({ required: true, default: SurveyCompleteStatus.PENDING })
+  completeStatus: SurveyCompleteStatus;
+
   @Prop({ default: 0, required: true })
   amountOfAnswers: number;
+
+  @Prop()
+  surveyResult: [any];
 
   @Prop({ type: [QuestionAnswerSchema], default: [], required: true })
   answers: QuestionAnswer[];

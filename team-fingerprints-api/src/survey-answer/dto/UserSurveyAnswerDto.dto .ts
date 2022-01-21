fbc384dcf8597cc4e9088dbd1ffe76dacdd4b672
cherryvue <yetiasg@gmail.com>
2022-01-21 12:57:46 +0100

    Apply ability to calculate survey result and change user survey status

--- team-fingerprints-api/src/survey-answer/dto/UserSurveyAnswerDto.dto .ts
+++
@@ -1,9 +0,0 @@
-import { Type } from 'class-transformer';
-import { ValidateNested } from 'class-validator';
-import { QuestionAnswerDto } from './QuestionAnswerDto.dto';
-
-export class UserSurveyAnswerDto {
-  @Type(() => QuestionAnswerDto)
-  @ValidateNested()
-  readonly answer: QuestionAnswerDto;
-}

3182fe0e5b530f537f2f28bc24328a4a2cd69e97
cherryvue <yetiasg@gmail.com>
2022-01-25 16:09:20 +0100

    Create role module, Apply new module in project

--- team-fingerprints-api/src/survey/survey.controller.ts
+++ team-fingerprints-api/src/survey/survey.controller.ts
@@ -10,9 +10,9 @@ import {
 } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { CurrentUserRole } from 'src/common/decorators/currentUserRole.decorator';
-import { RoleGuard } from 'src/common/decorators/role.guard';
+import { RoleGuard } from 'src/role/role.guard';
 import { ValidateObjectId } from 'src/common/pipes/ValidateObjectId.pipe';
-import { UserRole } from 'src/users/user.type';
+import { Role } from 'src/role/role.type';
 import { CreateSurveyDto } from './dto/CreateSurveyDto.dto';
 import { UpdateSurveyDto } from './dto/UpdateSurveyDto.dto';
 import { Survey } from './entities/survey.entity';
@@ -24,26 +24,26 @@ export class SurveyController {
   constructor(private readonly surveyService: SurveyService) {}
 
   @Get()
-  async getSurveysByRole(@CurrentUserRole() role: UserRole): Promise<Survey[]> {
+  async getSurveysByRole(@CurrentUserRole() role: Role): Promise<Survey[]> {
     return await this.surveyService.getSurveysByRole(role);
   }
 
   @Get('/:surveyId')
   async getSurvey(
     @Param('surveyId', ValidateObjectId) surveyId: string,
-    @CurrentUserRole() role: UserRole,
+    @CurrentUserRole() role: Role,
   ): Promise<Survey> {
     return this.surveyService.getSurvey(surveyId, role);
   }
 
   @Post()
-  @UseGuards(RoleGuard([UserRole.COMPANY_ADMIN]))
+  @UseGuards(RoleGuard([Role.SUPER_ADMIN]))
   async createSurvey(@Body() body: CreateSurveyDto): Promise<Survey> {
     return await this.surveyService.createSurvey(body);
   }
 
   @Patch('/:surveyId')
-  @UseGuards(RoleGuard([UserRole.COMPANY_ADMIN]))
+  @UseGuards(RoleGuard([Role.SUPER_ADMIN]))
   async updateSurvey(
     @Param('surveyId', ValidateObjectId) surveyId: string,
     @Body() body: UpdateSurveyDto,
@@ -52,7 +52,7 @@ export class SurveyController {
   }
 
   @Delete('/:surveyId')
-  @UseGuards(RoleGuard([UserRole.COMPANY_ADMIN]))
+  @UseGuards(RoleGuard([Role.SUPER_ADMIN]))
   async removeSurvey(
     @Param('surveyId', ValidateObjectId) surveyId: string,
   ): Promise<Survey> {

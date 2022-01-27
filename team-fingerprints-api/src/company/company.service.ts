aaaa81f456d9068633c34343f385b795a631d551
cherryvue <yetiasg@gmail.com>
2022-01-27 10:35:56 +0100

    Separate company members methods to new service

--- team-fingerprints-api/src/company/company.service.ts
+++ team-fingerprints-api/src/company/company.service.ts
@@ -2,15 +2,14 @@ import {
   ForbiddenException,
   HttpException,
   Injectable,
-  InternalServerErrorException,
   NotFoundException,
-  UnauthorizedException,
 } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { RoleService } from 'src/role/role.service';
 import { Role } from 'src/role/role.type';
 import { UsersService } from 'src/users/users.service';
+import { CompanyMembersService } from './company-members.service';
 import { CreateCompanyDto, UpdateCompanyDto } from './dto/company.dto';
 import { Company } from './entities/Company.entity';
 
@@ -20,6 +19,7 @@ export class CompanyService {
     @InjectModel(Company.name) private readonly companyModel: Model<Company>,
     private readonly usersService: UsersService,
     private readonly roleService: RoleService,
+    private readonly companyMembersService: CompanyMembersService,
   ) {}
   async getCompaneis(): Promise<Company[]> {
     return await this.companyModel.find({}).exec();
@@ -43,7 +43,7 @@ export class CompanyService {
     const { email } = await this.usersService.getUser(userId);
     if (!email) return new NotFoundException();
 
-    if (await this.isUserInAnyCompanyWhitelist(email)) {
+    if (await this.companyMembersService.isUserInAnyCompanyWhitelist(email)) {
       return new ForbiddenException(`You already belong to ${domain} company.`);
     }
 
@@ -82,69 +82,5 @@ export class CompanyService {
   async isDomainTaken(domain: string): Promise<boolean> {
     const companyByDomain = await this.companyModel.findOne({ domain }).exec();
     return companyByDomain ? true : false;
-  }
-
-  async isUserInAnyCompanyWhitelist(email: string): Promise<Company> {
-    const company = await this.companyModel.findOne({ emailWhitelist: email });
-    return company;
-  }
-
-  async isUserInCompanyDomain(email: string): Promise<Company> {
-    const company = await this.companyModel.findOne({
-      domain: email.split('@')[1],
-    });
-    return company;
-  }
-
-  async addUserToCompanyWhitelist(
-    company: string,
-    email: string,
-    currentUser: string,
-  ): Promise<Company | HttpException> {
-    if (await this.isUserInAnyCompanyWhitelist(email)) {
-      return new ForbiddenException();
-    }
-
-    const { companyId } = await this.usersService.getUser(currentUser);
-    if (companyId && companyId !== company) return new UnauthorizedException();
-
-    return await this.companyModel.findOneAndUpdate(
-      { _id: companyId },
-      { $push: { emailWhitelist: email } },
-      { new: true },
-    );
-  }
-
-  async addMemberToCompanyByEmail(
-    email: string,
-  ): Promise<Company | HttpException> {
-    const destinationCompnay =
-      (await this.isUserInAnyCompanyWhitelist(email)) ||
-      (await this.isUserInCompanyDomain(email));
-
-    if (!destinationCompnay) {
-      return new NotFoundException();
-    }
-
-    const newMember = await this.usersService.getUserByEmail(email);
-    if (!newMember) return new NotFoundException();
-
-    const newMemberId = newMember?._id.toString();
-
-    const members = destinationCompnay.members || [];
-
-    if (members.find((el) => el === newMemberId)) {
-      return new ForbiddenException();
-    }
-
-    const companyWithNewMember = await this.companyModel
-      .findOneAndUpdate(
-        { _id: destinationCompnay._id },
-        { $push: { members: newMemberId } },
-        { new: true },
-      )
-      .exec();
-    if (!companyWithNewMember) return new InternalServerErrorException();
-    return companyWithNewMember;
   }
 }

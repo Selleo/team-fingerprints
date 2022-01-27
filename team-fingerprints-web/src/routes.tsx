527406c571f5d80b582bfdd32013ad8cdaf00a17
tobob <bozek.wojciech@gmail.com>
2022-01-27 19:54:51 +0100

    New way of navigation

--- team-fingerprints-web/src/routes.tsx
+++ team-fingerprints-web/src/routes.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import MainRoute from "./routes/MainRoute";
 import Surveys from "./routes/Surveys";
 import { AppShell } from "@mantine/core";
@@ -12,41 +12,90 @@ import Users from "./routes/Users";
 import Companies from "./routes/Companies";
 import CompaniesNew from "./routes/Companies/New";
 
-import useUser from "./hooks/useUser";
+import CompaniesManagment from "./routes/Companies/Managment";
+import { Profile } from "./types/models";
+import { createContext, useEffect } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import { useQuery } from "react-query";
+import axios from "axios";
+import { queryClient } from "./App";
+
+interface ProfileContextInterface {
+  profile: Profile | undefined;
+}
+
+export const ProfileContext = createContext<ProfileContextInterface>(
+  {} as ProfileContextInterface
+);
 
 const AppRoutes = () => {
-  const { profile } = useUser();
-  const shouldSeeNav = profile?.role === "SUPER_ADMIN";
+  const { user, isAuthenticated } = useAuth0();
+  const navigate = useNavigate();
+
+  const { data, refetch } = useQuery<Profile>(
+    "profileData",
+    async () => {
+      const response = await axios.get<Profile>("/auth/profile");
+      return response.data;
+    },
+    {
+      refetchOnWindowFocus: false,
+      enabled: false,
+    }
+  );
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      refetch().then((data) => {
+        if (data.data?.canCreateTeam) {
+          navigate("companies/new");
+          return;
+        }
+        if (data.data?.role === "COMPANY_ADMIN") {
+          navigate("companies/managment");
+          return;
+        }
+      });
+    }
+    return () => {
+      queryClient.invalidateQueries(["profileData"]);
+    };
+  }, [isAuthenticated]);
+
+  const shouldSeeNav = data?.role === "SUPER_ADMIN";
 
   return (
-    <AppShell
-      // navbarOffsetBreakpoint controls when navbar should no longer be offset with padding-left
-      navbarOffsetBreakpoint="sm"
-      fixed
-      navbar={shouldSeeNav ? <AppNavBar /> : undefined}
-      header={<AppHeader />}
-    >
-      <Routes>
-        <Route path="/" element={<Surveys />} />
-        <Route path="surveys" element={<Surveys />} />
-        <Route path="companies" element={<Companies />} />
-        <Route path="companies/new" element={<CompaniesNew />} />
+    <ProfileContext.Provider value={{ profile: data }}>
+      <AppShell
+        // navbarOffsetBreakpoint controls when navbar should no longer be offset with padding-left
+        navbarOffsetBreakpoint="sm"
+        fixed
+        navbar={shouldSeeNav ? <AppNavBar /> : undefined}
+        header={<AppHeader />}
+      >
+        <Routes>
+          <Route path="/" element={<MainRoute />} />
+          <Route path="surveys" element={<Surveys />} />
+          <Route path="companies" element={<Companies />} />
+          <Route path="companies/new" element={<CompaniesNew />} />
+          <Route path="companies/managment" element={<CompaniesManagment />} />
 
-        <Route path="users" element={<Users />} />
+          <Route path="users" element={<Users />} />
 
-        <Route path="survey/:id" element={<SurveyDetails />} />
-        <Route
-          path="*"
-          element={
-            <main style={{ padding: "1rem" }}>
-              <p>There's nothing here!</p>
-            </main>
-          }
-        />
-        <Route path="responses" element={<Responses />} />
-        <Route path="response/:surveyId" element={<ResponseEdit />} />
-      </Routes>
-    </AppShell>
+          <Route path="survey/:id" element={<SurveyDetails />} />
+          <Route
+            path="*"
+            element={
+              <main style={{ padding: "1rem" }}>
+                <p>There's nothing here!</p>
+              </main>
+            }
+          />
+          <Route path="responses" element={<Responses />} />
+          <Route path="response/:surveyId" element={<ResponseEdit />} />
+        </Routes>
+      </AppShell>
+    </ProfileContext.Provider>
   );
 };
 

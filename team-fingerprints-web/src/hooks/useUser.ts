92b8e02a18f61c170da7fc168c3897098e42890a
tobob <bozek.wojciech@gmail.com>
2022-01-27 19:18:52 +0100

    Change structure of navigation

--- team-fingerprints-web/src/hooks/useUser.ts
+++ team-fingerprints-web/src/hooks/useUser.ts
@@ -1,9 +1,37 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import axios from "axios";
+import { useEffect } from "react";
+import { useQuery } from "react-query";
+import { Profile } from "../types/models";
+import { useNavigate } from "react-router-dom";
 
 const useUser = () => {
-  const { user } = useAuth0();
-  console.log(user);
-  return { user };
+  const { user, isAuthenticated } = useAuth0();
+  const navigate = useNavigate();
+
+  const { data, refetch } = useQuery<Profile>(
+    "profileData",
+    async () => {
+      const response = await axios.get<Profile>("/auth/profile");
+      return response.data;
+    },
+    {
+      refetchOnWindowFocus: false,
+      enabled: false,
+    }
+  );
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      refetch().then((data) => {
+        if (data.data?.canCreateTeam) {
+          navigate("companies/new");
+        }
+      });
+    }
+  }, [isAuthenticated]);
+
+  return { user, profile: data, isAuthenticated };
 };
 
 export default useUser;

e57ad876064ff030a8721db66a1d53bd427fabcf
cherryvue <yetiasg@gmail.com>
2022-02-01 08:08:50 +0100

    Handle amount of questions while removing questions, trends and categories

--- team-fingerprints-api/src/survey/category/trend/trend.service.ts
+++ team-fingerprints-api/src/survey/category/trend/trend.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Survey } from 'src/survey/models/survey.model';
@@ -7,11 +7,13 @@ import {
   TrendParamsDto,
   UpdateTrendDto,
 } from './dto/trend.dto';
+import { QuestionService } from './question/question.service';
 
 @Injectable()
 export class TrendService {
   constructor(
     @InjectModel(Survey.name) private readonly surveyModel: Model<Survey>,
+    private readonly questionService: QuestionService,
   ) {}
 
   async createTrend(
@@ -61,7 +63,32 @@ export class TrendService {
     );
   }
 
-  async removeTrend(trendId: string): Promise<Survey> {
+  async removeTrend({ surveyId, categoryId, trendId }: TrendParamsDto) {
+    const { categories }: any = await this.surveyModel.findOne(
+      {
+        _id: surveyId,
+      },
+      { categories: true },
+    );
+
+    if (!categories) return new NotFoundException();
+
+    const questionsId = [];
+    categories.forEach((category) => {
+      if (category?._id.toString() === categoryId) {
+        category.trends.forEach((trend) => {
+          if (trend?._id.toString() === trendId) {
+            trend.questions.forEach((question) => {
+              questionsId.push(question?._id.toString());
+            });
+          }
+        });
+      }
+    });
+    if (questionsId) {
+      await this.questionService.removeQuestions(surveyId, questionsId);
+    }
+
     return await this.surveyModel.findOneAndUpdate(
       {
         'categories.trends._id': trendId,

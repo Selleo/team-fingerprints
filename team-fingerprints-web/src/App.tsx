bbf40ebab83f54cfa1124adac83bd1b15c7de307
Mateusz <m.dobija@selleo.com>
2022-02-28 15:22:28 +0100

    styling: sass project configuration

--- team-fingerprints-web/src/App.tsx
+++ team-fingerprints-web/src/App.tsx
@@ -2,22 +2,24 @@ import { Auth0Provider } from "@auth0/auth0-react";
 import { QueryClient } from "react-query";
 import LoginGateway from "./LoginGateway";
 
+import './stylesheets/application.sass'
+
 export const queryClient = new QueryClient();
 
-const App = () => {
-  return (
-    <Auth0Provider
-      domain={process.env.REACT_APP_AUTH0_DOMAIN as string}
-      clientId={process.env.REACT_APP_AUTH0_CLIENT_ID as string}
-      redirectUri={window.location.origin}
-      scope="read"
-      audience={process.env.REACT_APP_AUTH0_AUDIENCE}
-      useRefreshTokens
-      cacheLocation="localstorage"
-    >
-      <LoginGateway />
-    </Auth0Provider>
+const App = () => (
+    <div className="app">
+      <Auth0Provider
+        domain={process.env.REACT_APP_AUTH0_DOMAIN as string}
+        clientId={process.env.REACT_APP_AUTH0_CLIENT_ID as string}
+        redirectUri={window.location.origin}
+        scope="read"
+        audience={process.env.REACT_APP_AUTH0_AUDIENCE}
+        useRefreshTokens
+        cacheLocation="localstorage"
+        >
+        <LoginGateway />
+      </Auth0Provider>
+    </div>
   );
-};
 
 export default App;

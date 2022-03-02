dc6a6f60cf69fa48d37c5877fbd6edcefdffa973
Mateusz Żupa <yetiasg@gmail.com>
2022-03-02 15:57:13 +0100

    Fix bug occurring until testing

--- team-fingerprints-api/test/company.e2e-spec.ts
+++ team-fingerprints-api/test/company.e2e-spec.ts
@@ -4,6 +4,9 @@ import * as request from 'supertest';
 import { CompanyModule } from 'src/company/company.module';
 import { AppModule } from 'src/app.module';
 import { Role } from 'src/role/role.type';
+import * as mongoose from 'mongoose';
+
+jest.setTimeout(40000);
 
 const authToken =
   'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlM0ZVVpMFVfdVlnYlpjbV9fSXBDYSJ9.eyJpc3MiOiJodHRwczovL2Rldi1sbGt0ZTQxbS51cy5hdXRoMC5jb20vIiwic3ViIjoiZ29vZ2xlLW9hdXRoMnwxMDU5ODkyODA1NTgxNDkzOTIyMjIiLCJhdWQiOlsiaHR0cDovL2xvY2FsaG9zdDozMDAwLyIsImh0dHBzOi8vZGV2LWxsa3RlNDFtLnVzLmF1dGgwLmNvbS91c2VyaW5mbyJdLCJpYXQiOjE2NDYyMjk3NzMsImV4cCI6MTY0NjMxNjE3MywiYXpwIjoiZ3FHWVFOaXM2SldmbXBkTlE1dG14Vlc5N1NHUHp6dmwiLCJzY29wZSI6Im9wZW5pZCBwcm9maWxlIGVtYWlsIn0.PAScPRv6BByemh241YgIvjcgTPddNKht6O44hwh0t_m9Ra7IuwjcyAk4WAuVeUiAkVxx_tNKSeGfmRhAzEZI2Ruuoop1Zo4pA7FVfsIZujtZ8j6mb8bVBDWypJXKeAdOURt5jtP5QIzujrPAzbeVCh1_L0rSxHX2OfbRROqpDVwDR-fioBaEvdLnUmDb7AhATJ7vVAMy7DNTogjzdE1hGwreuU6b5nx4lxgrpEbuYLwM2jVMpUJWZGM0_WHkokv-x44LbaVRXn71LEssQHyZ8lqToYUDP56BrTb35jBw3u8qfW9e70cn6cS2Hc22ol536QKheEHF6zt4nSzmbsukhg';
@@ -20,13 +23,10 @@ const mockCompanyData = {
   domain: 'selleo.com',
 };
 
-jest.setTimeout(40000);
-
 describe('Company controller (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
-    jest.setTimeout(10000);
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule, CompanyModule],
     }).compile();
@@ -252,6 +252,7 @@ describe('Company controller (e2e)', () => {
   });
 
   afterAll(async () => {
+    await mongoose.connection.close(true);
     await app.close();
   });
 });

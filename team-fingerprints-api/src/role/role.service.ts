8bf1563537898ee016eb4d079705638a066c1be3
Mateusz Żupa <yetiasg@gmail.com>
2022-03-10 15:46:44 +0100

    Handle user roles

--- team-fingerprints-api/src/role/role.service.ts
+++ team-fingerprints-api/src/role/role.service.ts
@@ -6,6 +6,7 @@ import {
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from 'src/users/models/user.model';
+import { RoleI } from './interfaces/role.interface';
 import { Role } from './models/role.model';
 import { RoleType } from './role.type';
 
@@ -27,10 +28,43 @@ export class RoleService {
     throw new BadRequestException();
   }
 
-  async createRoleDocument(userId: string, email: string): Promise<Role> {
-    const roleDocument = await this.roleModel.create({ userId, email });
+  async createRoleDocument({ email }: User): Promise<Role> {
+    const roleDocument = await this.roleModel.create({ email });
     if (!roleDocument) throw new InternalServerErrorException();
     await roleDocument.save();
     return roleDocument;
+  }
+
+  async findOneRoleDocument(searchParams: Partial<RoleI>): Promise<Role> {
+    const roleDocument = await this.roleModel.findOne(searchParams).exec();
+    if (!roleDocument) throw new BadRequestException();
+    return roleDocument;
+  }
+
+  async findAllRoleDocuments(searchParams: Partial<RoleI>): Promise<Role[]> {
+    const roleDocuments = await this.roleModel.find(searchParams).exec();
+    if (!roleDocuments || roleDocuments.length <= 0) return [];
+    return roleDocuments;
+  }
+
+  async updateRoleDocument(
+    searchParams: Partial<RoleI>,
+    updateData: Partial<RoleI>,
+  ): Promise<Role> {
+    const updatedRoleDocument = await this.roleModel
+      .findOneAndUpdate({ searchParams }, { updateData }, { new: true })
+      .exec();
+    if (!updatedRoleDocument)
+      throw new InternalServerErrorException('Could not update role document');
+
+    return updatedRoleDocument;
+  }
+
+  async removeRoleDocumentById(roleDocument: Role): Promise<Role> {
+    return await this.roleModel
+      .findByIdAndDelete(roleDocument._id, {
+        new: true,
+      })
+      .exec();
   }
 }

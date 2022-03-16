a477166d4614bf05ca3058989241fcdf7cf6773e
Mateusz Żupa <yetiasg@gmail.com>
2022-03-16 14:17:12 +0100

    Fix currenUserRole decorator bug

--- team-fingerprints-api/src/common/decorators/currentUserRole.decorator.ts
+++ team-fingerprints-api/src/common/decorators/currentUserRole.decorator.ts
@@ -3,6 +3,6 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 export const CurrentUserRole = createParamDecorator(
   async (data: unknown, context: ExecutionContext) => {
     const request = context.switchToHttp().getRequest() as any;
-    return request?.user?.role;
+    return request?.roleDocument?.role;
   },
 );

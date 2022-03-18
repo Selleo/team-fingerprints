a93438aa9c26d2c8ce87527cf48f5a11c8ebfa51
cherryvue <yetiasg@gmail.com>
2022-03-18 14:54:04 +0100

    New role handler

--- team-fingerprints-api/src/role/interceptors/Privileges.interceptor.ts
+++ team-fingerprints-api/src/role/interceptors/Privileges.interceptor.ts
@@ -20,38 +20,70 @@ export class PrivilegesInterceptor implements NestInterceptor {
   async intercept(context: ExecutionContext, handler: CallHandler) {
     const request = context.switchToHttp().getRequest();
     const email = request.user.email;
-    const { companyId } = request.params;
+    const params = request.params;
 
-    const roleDocuments = await this.roleService.findAllRoleDocuments({
-      email,
-      companyId,
-    });
+    const companyId = params.companyId ?? null;
+    const teamId = params.teamId ?? null;
 
-    if (!roleDocuments || roleDocuments.length <= 0)
-      throw new UnauthorizedException();
-
-    if (roleDocuments.some((el) => el.role === RoleType.SUPER_ADMIN)) {
-      request.roleDocument = {
-        ...request.roleDocument,
-        role: RoleType.SUPER_ADMIN,
-      };
-    } else if (roleDocuments.some((el) => el.role === RoleType.COMPANY_ADMIN)) {
-      request.roleDocument = {
-        ...request.roleDocument,
-        role: RoleType.COMPANY_ADMIN,
-      };
-    } else if (roleDocuments.some((el) => el.role === RoleType.TEAM_LEADER)) {
-      request.roleDocument = {
-        ...request.roleDocument,
+    // Handle USER && TEAM LEADER
+    if (companyId && teamId) {
+      const leaderRoleDocument = await this.roleService.findOneRoleDocument({
+        email,
+        companyId,
+        teamId,
         role: RoleType.TEAM_LEADER,
-      };
-    } else {
-      request.roleDocument = {
-        ...request.roleDocument,
+      });
+      if (leaderRoleDocument) {
+        request.roleDocument = leaderRoleDocument;
+        return handler.handle();
+      }
+
+      const userRoleDocument = await this.roleService.findOneRoleDocument({
+        email,
+        companyId,
+        teamId,
         role: RoleType.USER,
-      };
+      });
+      if (leaderRoleDocument) {
+        request.roleDocument = userRoleDocument;
+        return handler.handle();
+      }
     }
 
-    return handler.handle();
+    // Handle COMPANY_ADMIN
+
+    if (companyId && (!teamId || teamId.length <= 0)) {
+      const companyAdminRoleDocument =
+        await this.roleService.findOneRoleDocument({
+          email,
+          companyId,
+          role: RoleType.COMPANY_ADMIN,
+        });
+
+      if (companyAdminRoleDocument) {
+        request.roleDocument = companyAdminRoleDocument;
+        return handler.handle();
+      }
+    }
+
+    // Handle SUPER_ADMIN
+    if (
+      (!companyId || companyId.length <= 0) &&
+      (!teamId || teamId.length <= 0)
+    ) {
+      const superAdminRoleDocument = await this.roleService.findOneRoleDocument(
+        {
+          email,
+          role: RoleType.SUPER_ADMIN,
+        },
+      );
+
+      if (superAdminRoleDocument) {
+        request.roleDocument = superAdminRoleDocument;
+        return handler.handle();
+      }
+    }
+
+    throw new UnauthorizedException();
   }
 }

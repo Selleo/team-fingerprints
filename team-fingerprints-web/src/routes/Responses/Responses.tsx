0c97357973e5bc27138959847e154e8ab0cef76d
Adam Jachymek <jachymek@outlook.com>
2022-03-21 15:48:54 +0100

    closing #144 final version: filter-only-public-surveys-on-an-answer-route

--- team-fingerprints-web/src/routes/Responses/Responses.tsx
+++ team-fingerprints-web/src/routes/Responses/Responses.tsx
@@ -24,7 +24,7 @@ const Responses = () => {
     }
   );
 
-  const filtredByPublic = data?.filter((survey) => survey.isPublic===true)
+  const filtredByPublic = data?.filter((survey) => survey.isPublic)
 
   useEffect(() => {
     if (data?.length === 1) {

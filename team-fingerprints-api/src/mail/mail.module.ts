9c48c9552f875c9da24c39bd3ebb1c5606e1e146
Mateusz Żupa <yetiasg@gmail.com>
2022-03-22 13:45:31 +0100

    WIP: Setup mailing module

--- team-fingerprints-api/src/mail/mail.module.ts
+++ team-fingerprints-api/src/mail/mail.module.ts
@@ -1,10 +1,53 @@
+import { MailerModule } from '@nestjs-modules/mailer';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import { BullModule } from '@nestjs/bull';
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MailProcessor } from './mail.processor';
 import { MailService } from './mail.service';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ConfigModule],
-  providers: [MailService],
+  imports: [
+    ConfigModule,
+    MailerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        transport: {
+          service: configService.get<string>('MAIL_SERVICE'),
+          host: configService.get<string>('MAIL_HOST'),
+          secure: false,
+          auth: {
+            user: configService.get<string>('MAIL_USER'),
+            pass: configService.get<string>('MAIL_PASS'),
+          },
+        },
+        defaults: {
+          from: 'fingerprints@selleo.com',
+        },
+        template: {
+          dir: process.cwd() + '/templates',
+          adapter: new HandlebarsAdapter(),
+          options: {
+            strict: true,
+          },
+        },
+      }),
+    }),
+    BullModule.registerQueueAsync({
+      name: 'mailsend',
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        redis: {
+          port: 6379,
+          host: configService.get<string>('REDIS_HOST') ?? 'localhost',
+          password: configService.get<string>('REDIS_PASSWORD') ?? 'redis',
+        },
+      }),
+    }),
+  ],
+  providers: [MailService, MailProcessor],
   exports: [MailService],
 })
 export class MailModule {}

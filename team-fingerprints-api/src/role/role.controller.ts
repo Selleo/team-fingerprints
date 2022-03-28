961b975fd4811cd6e428c70fb802984e49f0f593
Mateusz Żupa <yetiasg@gmail.com>
2022-03-28 12:06:37 +0200

    Add ability to give other user super_admin role

--- team-fingerprints-api/src/role/role.controller.ts
+++ team-fingerprints-api/src/role/role.controller.ts
@@ -1,13 +1,21 @@
-import { Controller, Delete, Param } from '@nestjs/common';
+import { Body, Controller, Delete, Param, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { ValidateObjectId } from 'src/common/pipes/ValidateObjectId.pipe';
 import { RoleService } from './role.service';
 import { CurrentUserId } from 'src/common/decorators/currentUserId.decorator';
+import { Roles } from './decorators/roles.decorator';
+import { RoleType } from './role.type';
 
 @ApiTags('role')
 @Controller({ path: 'role', version: '1' })
 export class RoleController {
   constructor(private readonly roleService: RoleService) {}
+
+  @Post('superAdmin')
+  @Roles([RoleType.SUPER_ADMIN])
+  async addSuperAdmin(@Body('email') email: string) {
+    return await this.roleService.addSuperAdmin(email);
+  }
 
   @Delete('/:roleId/leave')
   async leave(

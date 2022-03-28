02e87c32b8bf0e4cd916f4ae87a25fb81bf0a465
Mateusz Żupa <yetiasg@gmail.com>
2022-03-28 11:32:05 +0200

    Add user to company by domain only while firs log in

--- team-fingerprints-api/src/users/interfaces/user.interface.ts
+++ team-fingerprints-api/src/users/interfaces/user.interface.ts
@@ -14,6 +14,7 @@ export interface UserI {
   pictureUrl?: string;
   surveysAnswers: UserSurveyAnswerI[];
   userDetails?: UserDetailI[];
+  inCompany: boolean;
 }
 
 export interface UserSurveyAnswerI {

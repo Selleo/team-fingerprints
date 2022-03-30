4a49e6467855c2466f8e2a610efc92d7e3f400a0
Mateusz Żupa <yetiasg@gmail.com>
2022-03-30 09:12:45 +0200

    Ability to get available filter values per companies / company / team

--- team-fingerprints-api/src/survey-result/survey-result.module.ts
+++ team-fingerprints-api/src/survey-result/survey-result.module.ts
@@ -2,10 +2,12 @@ import { forwardRef, Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CompanyModule } from 'src/company/company.module';
 import { TeamModule } from 'src/company/team/team.module';
+import { FilterModule } from 'src/filter/filter.module';
 import { RoleModule } from 'src/role/role.module';
 import { SurveyAnswerModule } from 'src/survey-answer/survey-answer.module';
 import { Survey, SurveySchema } from 'src/survey/models/survey.model';
 import { User, UserSchema } from 'src/users/models/user.model';
+import { UsersModule } from 'src/users/users.module';
 import { SurveyResultController } from './survey-result.controller';
 import { SurveyResultService } from './survey-result.service';
 
@@ -15,6 +17,8 @@ import { SurveyResultService } from './survey-result.service';
     forwardRef(() => RoleModule),
     TeamModule,
     CompanyModule,
+    UsersModule,
+    FilterModule,
     MongooseModule.forFeature([
       {
         name: User.name,

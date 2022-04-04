40174d962aa06350bf9c671e7dbb6930258d9a87
Mateusz Żupa <yetiasg@gmail.com>
2022-04-04 09:42:33 +0200

    Test e2e surveys controller - GET /surveys

--- team-fingerprints-api/test/survey.e2e-spec.ts
+++ team-fingerprints-api/test/survey.e2e-spec.ts
@@ -1,20 +1,17 @@
 import { INestApplication } from '@nestjs/common';
 import { getModelToken } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { User } from 'src/users/models/user.model';
 import { getApplication } from './helpers/getApplication';
-import { getBaseUser } from './helpers/getBaseUser';
 import * as request from 'supertest';
+import { Survey } from 'src/survey/models/survey.model';
 
 describe('SurveyController', () => {
   let app: INestApplication;
-  let baseUser: User;
-  let userModel: Model<User>;
+  let surveyModel: Model<Survey>;
 
   beforeEach(async () => {
     app = await getApplication();
-    userModel = app.get(getModelToken(User.name));
-    baseUser = await getBaseUser(userModel);
+    surveyModel = app.get(getModelToken(Survey.name));
   });
 
   describe('GET /surveys - get survey with complete status for current user', () => {
@@ -25,6 +22,36 @@ describe('SurveyController', () => {
 
       expect(body.length).toBe(0);
       expect(body).toEqual([]);
+    });
+
+    it('returns array with one survey', async () => {
+      const surveyData = {
+        title: 'Test survey',
+        isPublic: true,
+      };
+      const newSurvey = await (await surveyModel.create(surveyData)).save();
+
+      const { body } = await request(app.getHttpServer())
+        .get('/surveys')
+        .expect(200);
+
+      expect(body.length).toBe(1);
+
+      const {
+        _id,
+        title,
+        completeStatus,
+        isPublic,
+        amountOfQuestions,
+        categories,
+      } = body[0];
+
+      expect(_id).toEqual(newSurvey._id.toString());
+      expect(title).toBe(newSurvey.title);
+      expect(completeStatus).toEqual('new');
+      expect(isPublic).toBe(true);
+      expect(amountOfQuestions).toBe(0);
+      expect(categories).toEqual([]);
     });
   });
 });

b78600539fa8e51e5f1d24d2c4bb6a0adaaba457
Mateusz Żupa <yetiasg@gmail.com>
2022-04-04 12:01:04 +0200

    Test e2e trends controller - POST /surveys/:surveyId/categories/:categoryId/trends

---
+++ team-fingerprints-api/test/trend.e2e-spec.ts
@@ -0,0 +1,59 @@
+import { INestApplication } from '@nestjs/common';
+import { getModelToken } from '@nestjs/mongoose';
+import { Model, Types } from 'mongoose';
+import { Survey } from 'src/survey/models/survey.model';
+import { getApplication } from './helpers/getApplication';
+import * as request from 'supertest';
+import { CreateTrendDto } from 'src/survey/category/trend/dto/trend.dto';
+
+const createSurveyWithCategory = async (
+  surveyModel: Model<Survey>,
+): Promise<Survey> => {
+  const surveyData = {
+    title: 'Test survey',
+    categories: [
+      {
+        _id: new Types.ObjectId(),
+        title: 'Test category',
+        trends: [],
+      },
+    ],
+  };
+  return await (await surveyModel.create(surveyData)).save();
+};
+
+describe('TrendController', () => {
+  let app: INestApplication;
+  let surveyModel: Model<Survey>;
+  let survey: Survey;
+
+  beforeEach(async () => {
+    app = await getApplication();
+    surveyModel = app.get(getModelToken(Survey.name));
+    survey = await createSurveyWithCategory(surveyModel);
+  });
+
+  describe('POST /surveys/:surveyId/categories/:categoryId/trends - add new trend to category', () => {
+    it('returns survey with category that contains new trend', async () => {
+      const { _id, categories } = survey;
+
+      const trendData: CreateTrendDto = {
+        primary: 'Primary',
+        secondary: 'Secondary',
+      };
+
+      const { body } = await request(app.getHttpServer())
+        .post(
+          `/surveys/${_id.toString()}/categories/${categories[0]._id.toString()}/trends`,
+        )
+        .send(trendData)
+        .expect(201);
+
+      const trends = body.categories[0].trends;
+
+      expect(trends.length).toBe(1);
+      expect(trends[0].primary).toEqual(trendData.primary);
+      expect(trends[0].secondary).toEqual(trendData.secondary);
+    });
+  });
+});

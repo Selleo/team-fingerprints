2562a8f0449b671b51a0954ef060475430b47437
Mateusz Żupa <yetiasg@gmail.com>
2022-04-04 09:50:49 +0200

    Test e2e surveys controller - GET /surveys/public, Check 'archived' property for GET /surveys

--- team-fingerprints-api/test/survey.e2e-spec.ts
+++ team-fingerprints-api/test/survey.e2e-spec.ts
@@ -14,7 +14,7 @@ describe('SurveyController', () => {
     surveyModel = app.get(getModelToken(Survey.name));
   });
 
-  describe('GET /surveys - get survey with complete status for current user', () => {
+  describe('GET /surveys - get surveys with complete status for current user', () => {
     it('returns empty array', async () => {
       const { body } = await request(app.getHttpServer())
         .get('/surveys')
@@ -42,6 +42,7 @@ describe('SurveyController', () => {
         title,
         completeStatus,
         isPublic,
+        archived,
         amountOfQuestions,
         categories,
       } = body[0];
@@ -50,6 +51,42 @@ describe('SurveyController', () => {
       expect(title).toBe(newSurvey.title);
       expect(completeStatus).toEqual('new');
       expect(isPublic).toBe(true);
+      expect(archived).toBe(false);
+      expect(amountOfQuestions).toBe(0);
+      expect(categories).toEqual([]);
+    });
+  });
+
+  describe('GET /surveys/public - get public surveys', () => {
+    it('returns empty array', async () => {
+      const { body } = await request(app.getHttpServer())
+        .get('/surveys/public')
+        .expect(200);
+
+      expect(body.length).toBe(0);
+      expect(body).toEqual([]);
+    });
+
+    it('returns array with one survey', async () => {
+      const surveyData = {
+        title: 'Test survey',
+        isPublic: true,
+      };
+      const newSurvey = await (await surveyModel.create(surveyData)).save();
+
+      const { body } = await request(app.getHttpServer())
+        .get('/surveys/public')
+        .expect(200);
+
+      expect(body.length).toBe(1);
+
+      const { _id, title, isPublic, archived, amountOfQuestions, categories } =
+        body[0];
+
+      expect(_id).toEqual(newSurvey._id.toString());
+      expect(title).toBe(newSurvey.title);
+      expect(isPublic).toBe(true);
+      expect(archived).toBe(false);
       expect(amountOfQuestions).toBe(0);
       expect(categories).toEqual([]);
     });

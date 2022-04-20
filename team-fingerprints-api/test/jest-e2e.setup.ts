9227874b1878e53f4363d7dc18e266ea34165005
Mateusz Żupa <yetiasg@gmail.com>
2022-04-20 12:29:11 +0200

    Update e2e tests setup

--- team-fingerprints-api/test/jest-e2e.setup.ts
+++ team-fingerprints-api/test/jest-e2e.setup.ts
@@ -1,5 +1,5 @@
 import { getModelToken } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Connection, Model } from 'mongoose';
 import { Role } from 'src/role/models/role.model';
 import { User } from 'src/users/models/user.model';
 import { createBaseUser, giveSuperAdminPrivileges } from './helpers/users';
@@ -12,13 +12,17 @@ jest.mock('cache-manager');
 let usersModel: Model<User>;
 let roleModel: Model<Role>;
 let baseUser: User;
+let connection: Connection;
 
 jest.setTimeout(40000);
 
+beforeAll(async () => {
+  connection = await getMongoConnection();
+});
+
 beforeEach(async () => {
   jest.setTimeout(20000);
-  const conn = await getMongoConnection();
-  await conn.dropDatabase();
+  await connection.dropDatabase();
 
   const app = await getApplication();
   usersModel = app.get(getModelToken(User.name));
@@ -28,9 +32,8 @@ beforeEach(async () => {
 });
 
 afterAll(async () => {
-  const conn = await getMongoConnection();
-  await conn.dropDatabase();
-  await conn.close();
+  await connection.dropDatabase();
+  await connection.close();
 
   const app = await getApplication();
   await app.close();

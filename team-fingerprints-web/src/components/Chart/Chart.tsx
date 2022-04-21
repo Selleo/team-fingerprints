cf3d51b893828d3390675f8f231079a218228a87
Adam Jachymek <jachymek@outlook.com>
2022-04-21 10:23:01 +0200

    closing #67: take a look if we scale at full hd screens

--- team-fingerprints-web/src/components/Chart/Chart.tsx
+++ team-fingerprints-web/src/components/Chart/Chart.tsx
@@ -57,7 +57,7 @@ const Chart: FC<IProps> = ({ surveyResult, additionalData, showMe }) => {
   useEffect(() => {
     setWidth(ref.current?.clientWidth);
     setHeight(ref.current?.clientHeight);
-  }, []);
+  }, [screenWidth]);
 
   const displayWidth = Math.floor(pixelRatio * width);
   const renderingAreaWidth = displayWidth - 50;
@@ -115,7 +115,7 @@ const Chart: FC<IProps> = ({ surveyResult, additionalData, showMe }) => {
         ctx.fillStyle = "#121212";
         switch (shape) {
           case "circle":
-            ctx.arc(dotPosition, positionOfLine, 18, 0, 2 * Math.PI, true);
+            ctx.arc(dotPosition, positionOfLine, 11, 0, 2 * Math.PI, true);
             break;
           case "triangle":
             ctx.moveTo(dotPosition, positionOfLine - 15);
@@ -129,11 +129,11 @@ const Chart: FC<IProps> = ({ surveyResult, additionalData, showMe }) => {
             ctx.lineTo(dotPosition - 15, positionOfLine);
             break;
           default:
-            ctx.rect(dotPosition - 12, positionOfLine - 12, 24, 24);
+            ctx.rect(dotPosition - 12, positionOfLine - 10, 20, 20);
         }
         ctx.closePath();
         ctx.fill();
-        ctx.lineWidth = 12;
+        ctx.lineWidth = 8;
         ctx.strokeStyle = color || "#c99284";
         ctx.stroke();
       });

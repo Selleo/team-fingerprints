503a91977a554a28db2d9f0a6e9d8ad14b6d9665
Mateusz Żupa <yetiasg@gmail.com>
2022-04-28 10:15:39 +0200

    Create class that mocks auth guard

---
+++ team-fingerprints-api/test/mocks/JwtAuthGuard.mock.ts
@@ -0,0 +1,17 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
+
+@Injectable()
+export class JwtAuthGuardMock implements CanActivate {
+  constructor(private readonly usersService: UsersService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const users = await this.usersService.getUsersAll();
+    const user = users[0];
+    if (user) {
+      request.user = user;
+      return !!user;
+    } else return true;
+  }
+}

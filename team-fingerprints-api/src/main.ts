a620762b6e1aa4e28c270baba25dca320cff754a
Mateusz Żupa <yetiasg@gmail.com>
2022-05-19 09:07:42 +0200

    Fix typo

--- team-fingerprints-api/src/main.ts
+++ team-fingerprints-api/src/main.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
       .setDescription('API documentation')
       .setVersion('1.0')
       .addBearerAuth()
-      .addTag('auth', 'Obtaining the currently logged in user profilee')
+      .addTag('auth', 'Obtaining the currently logged in user profile')
       .addTag('role', 'Handling roles')
       .addTag('users', 'Handle users')
       .addTag('companies', 'Add tag description')
